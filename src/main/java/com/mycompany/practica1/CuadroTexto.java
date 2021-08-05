1d5f4945ef8c7c3f25a82ed4763f0bbac4428b86
Fernando Rodriguez <FernandoJRR@github.com>
2021-08-04 18:03:17 -0600

    Interfaz mejorada

--- src/main/java/com/mycompany/practica1/CuadroTexto.java
+++ src/main/java/com/mycompany/practica1/CuadroTexto.java
@@ -29,31 +29,20 @@ public class CuadroTexto extends javax.swing.JFrame {
         java.awt.GridBagConstraints gridBagConstraints;
 
         jPanel1 = new javax.swing.JPanel();
-        expresionTextField = new javax.swing.JTextField();
         analizarButton = new javax.swing.JButton();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        tokensTextArea = new javax.swing.JTextArea();
         tokensLabel = new javax.swing.JLabel();
         expresionLabel = new javax.swing.JLabel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        tokensTextArea = new javax.swing.JTextArea();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        expresionTextArea = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setMinimumSize(new java.awt.Dimension(350, 250));
-        setPreferredSize(new java.awt.Dimension(350, 250));
+        setMinimumSize(new java.awt.Dimension(350, 350));
 
-        jPanel1.setMinimumSize(new java.awt.Dimension(350, 200));
-        jPanel1.setPreferredSize(new java.awt.Dimension(350, 200));
-        java.awt.GridBagLayout jPanel1Layout = new java.awt.GridBagLayout();
-        jPanel1Layout.columnWidths = new int[] {0, 5, 0};
-        jPanel1Layout.rowHeights = new int[] {0, 5, 0, 5, 0, 5, 0};
-        jPanel1.setLayout(jPanel1Layout);
-
-        expresionTextField.setPreferredSize(new java.awt.Dimension(100, 23));
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 2;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        gridBagConstraints.ipadx = 114;
-        jPanel1.add(expresionTextField, gridBagConstraints);
+        jPanel1.setMinimumSize(new java.awt.Dimension(380, 350));
+        jPanel1.setPreferredSize(new java.awt.Dimension(380, 350));
+        jPanel1.setLayout(new java.awt.GridBagLayout());
 
         analizarButton.setText("Analizar");
         analizarButton.addActionListener(new java.awt.event.ActionListener() {
@@ -62,45 +51,71 @@ public class CuadroTexto extends javax.swing.JFrame {
             }
         });
         gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 2;
-        gridBagConstraints.gridy = 2;
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.insets = new java.awt.Insets(8, 0, 0, 20);
         jPanel1.add(analizarButton, gridBagConstraints);
-
-        jScrollPane2.setPreferredSize(new java.awt.Dimension(274, 130));
-
-        tokensTextArea.setColumns(20);
-        tokensTextArea.setRows(5);
-        jScrollPane2.setViewportView(tokensTextArea);
-
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 6;
-        gridBagConstraints.gridwidth = 3;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        jPanel1.add(jScrollPane2, gridBagConstraints);
 
         tokensLabel.setText("Tokens");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 4;
-        gridBagConstraints.gridwidth = 3;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.insets = new java.awt.Insets(20, 0, 8, 0);
         jPanel1.add(tokensLabel, gridBagConstraints);
 
         expresionLabel.setText("Expresion");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
+        gridBagConstraints.insets = new java.awt.Insets(20, 0, 8, 0);
         jPanel1.add(expresionLabel, gridBagConstraints);
+
+        tokensTextArea.setEditable(false);
+        tokensTextArea.setColumns(20);
+        tokensTextArea.setRows(5);
+        jScrollPane3.setViewportView(tokensTextArea);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipadx = 258;
+        gridBagConstraints.ipady = 122;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(0, 20, 20, 20);
+        jPanel1.add(jScrollPane3, gridBagConstraints);
+
+        expresionTextArea.setColumns(20);
+        expresionTextArea.setRows(5);
+        jScrollPane1.setViewportView(expresionTextArea);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipadx = 258;
+        gridBagConstraints.ipady = 75;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(0, 20, 0, 20);
+        jPanel1.add(jScrollPane1, gridBagConstraints);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
@@ -108,7 +123,7 @@ public class CuadroTexto extends javax.swing.JFrame {
 
     private void analizarButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_analizarButtonActionPerformed
         tokensTextArea.setText("");
-        Token[] tokens = Tokenizador.tokenizar((expresionTextField.getText()+" ").toCharArray());
+        Token[] tokens = Tokenizador.tokenizar((expresionTextArea.getText()+" ").toCharArray());
         for (Token token : tokens) {
             tokensTextArea.setText(tokensTextArea.getText()+token.getTipoToken()+" "+token.getExpresion()+"\n");
         }
@@ -119,9 +134,10 @@ public class CuadroTexto extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton analizarButton;
     private javax.swing.JLabel expresionLabel;
-    private javax.swing.JTextField expresionTextField;
+    private javax.swing.JTextArea expresionTextArea;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JLabel tokensLabel;
     private javax.swing.JTextArea tokensTextArea;
     // End of variables declaration//GEN-END:variables
